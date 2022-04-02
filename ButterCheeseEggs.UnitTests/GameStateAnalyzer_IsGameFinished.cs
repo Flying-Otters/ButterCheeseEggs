1b34fc636d4edb8d24ad693460a652e9d71ed85f
Joost Haverkort <1299167+joostit@users.noreply.github.com>
2022-04-02 19:46:11 +0200

    Added unit tests for GameStateAnalyzer_IsGameFinished

---
+++ ButterCheeseEggs.UnitTests/GameStateAnalyzer_IsGameFinished.cs
@@ -0,0 +1,61 @@
+﻿using ButterCheeseEggs.Models;
+using ButterCheeseEggs.Services.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButterCheeseEggs.UnitTests
+{
+    [TestClass]
+    public class GameStateAnalyzer_IsGameFinished
+    {
+
+        [TestMethod]
+        public void WinnerXMakeGameFinished()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState()
+            {
+                Winner = Players.X
+            };
+
+            analyzer.IsGameFinished(state);
+
+            Assert.IsTrue(state.IsGameFinished);
+        }
+
+
+        [TestMethod]
+        public void WinnerOMakeGameFinished()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState()
+            {
+                Winner = Players.O
+            };
+
+            analyzer.IsGameFinished(state);
+
+            Assert.IsTrue(state.IsGameFinished);
+        }
+
+        [TestMethod]
+        public void NoWinnerWithEmptyTilesMakeGameNotFinished()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState()
+            {
+                Winner = Players.None
+            };
+
+            analyzer.IsGameFinished(state);
+
+            Assert.IsFalse(state.IsGameFinished);
+        }
+
+
+    }
+}
