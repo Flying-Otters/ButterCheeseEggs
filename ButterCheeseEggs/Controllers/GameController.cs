26e6097b318c6b22cd540926ae06b8eaf6e34f8b
Te3 <75424462+TeoVal@users.noreply.github.com>
2022-04-02 13:21:38 +0200

    Use button and create Views

---
+++ ButterCheeseEggs/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ButterCheeseEggs.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Play()
+        {
+            return View();
+        }
+    }
+}
