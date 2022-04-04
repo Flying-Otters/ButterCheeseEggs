72c13db78a302d6c11e8b98f8131c7bd259bd3a8
Te3 <75424462+TeoVal@users.noreply.github.com>
2022-04-04 19:25:01 +0200

    WIP: determining winner and Unittesting

---
+++ ButterCheeseEggs.UnitTests/GameStateAnalyzer_DetermineWinner.cs
@@ -0,0 +1,213 @@
+﻿using ButterCheeseEggs.Models;
+using ButterCheeseEggs.Services.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButterCheeseEggs.UnitTests
+{
+    [TestClass]
+    public class GameStateAnalyzer_DetermineWinner
+    {
+        [TestMethod]
+        public void WinnerIsNoneIfGameStateHasEmptyTiles()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.AreEqual(Players.None, result);
+        }
+
+
+        [TestMethod]
+        public void WinnerIsXIfRowIsInWinningConditionForX()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0,0] = TileStates.X;
+            state.Table[1,0] = TileStates.X;
+            state.Table[2,0] = TileStates.X;
+            state.Table[2, 1] = TileStates.O;
+            state.Table[2, 2] = TileStates.O;
+            state.Table[0, 1] = TileStates.O;
+            state.Table[1, 2] = TileStates.O;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.AreEqual(Players.X,result);
+        }
+
+
+        [TestMethod]
+        public void WinnerIsOIfRowIsInWinningConditionForO()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0, 0] = TileStates.O;
+            state.Table[1, 0] = TileStates.O;
+            state.Table[2, 0] = TileStates.O;
+            state.Table[2, 1] = TileStates.X;
+            state.Table[2, 2] = TileStates.X;
+            state.Table[0, 1] = TileStates.O;
+            state.Table[1, 2] = TileStates.X;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.AreEqual(Players.O, result);
+        }
+
+
+        [TestMethod]
+        public void WinnerIsXIfColumnIsInWinningConditionForX()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0, 0] = TileStates.O;
+            state.Table[1, 0] = TileStates.O;
+            state.Table[2, 0] = TileStates.X;
+            state.Table[2, 1] = TileStates.X;
+            state.Table[2, 2] = TileStates.X;
+            state.Table[0, 1] = TileStates.O;
+            state.Table[1, 2] = TileStates.X;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.AreEqual(Players.X, result);
+        }
+
+
+        [TestMethod]
+        public void WinnerIsOIfColumnIsInWinningConditionForO()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0, 0] = TileStates.O;
+            state.Table[2, 0] = TileStates.X;
+            state.Table[2, 1] = TileStates.X;
+            state.Table[2, 2] = TileStates.X;
+            state.Table[0, 1] = TileStates.O;
+            state.Table[1, 2] = TileStates.O;
+            state.Table[1, 0] = TileStates.O;
+            state.Table[1, 1] = TileStates.O;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.AreEqual(Players.O, result);
+        }
+
+
+
+        [TestMethod]
+        public void WinnerIsNotXIfOnlyTwoRowsInColumnAreX()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0, 0] = TileStates.O;
+            state.Table[2, 0] = TileStates.X;
+            state.Table[2, 1] = TileStates.O;
+            state.Table[2, 2] = TileStates.X;
+            state.Table[0, 1] = TileStates.O;
+            state.Table[1, 2] = TileStates.O;
+            state.Table[1, 0] = TileStates.X;
+            state.Table[1, 1] = TileStates.X;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.IsFalse(result == Players.X);
+        }
+
+
+        [TestMethod]
+        public void WinnerIsNotXIfOnlyTwoColumnsInRowAreX()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0, 0] = TileStates.O;
+            state.Table[2, 0] = TileStates.X;
+            state.Table[2, 1] = TileStates.O;
+            state.Table[2, 2] = TileStates.X;
+            state.Table[0, 1] = TileStates.X;
+            state.Table[1, 2] = TileStates.O;
+            state.Table[1, 0] = TileStates.X;
+            state.Table[1, 1] = TileStates.X;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.IsFalse(result == Players.X);
+        }
+
+        [TestMethod]
+        public void WinnerIsNotOIfOnlyTwoRowsInColumnAreO()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0, 0] = TileStates.O;
+            state.Table[2, 0] = TileStates.X;
+            state.Table[2, 1] = TileStates.O;
+            state.Table[2, 2] = TileStates.X;
+            state.Table[0, 1] = TileStates.X;
+            state.Table[1, 2] = TileStates.O;
+            state.Table[1, 0] = TileStates.X;
+            state.Table[1, 1] = TileStates.O;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.IsFalse(result == Players.O);
+        }
+
+
+        [TestMethod]
+        public void WinnerIsNotOIfOnlyTwoColumnsInRowAreO()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0, 0] = TileStates.O;
+            state.Table[2, 0] = TileStates.X;
+            state.Table[2, 1] = TileStates.O;
+            state.Table[2, 2] = TileStates.X;
+            state.Table[0, 1] = TileStates.X;
+            state.Table[1, 2] = TileStates.O;
+            state.Table[1, 0] = TileStates.X;
+            state.Table[1, 1] = TileStates.O;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.IsFalse(result == Players.O);
+        }
+
+
+        [TestMethod]
+        public void PlayerIsNoneIfGameIsATie()
+        {
+            GameStateAnalyzer analyzer = new GameStateAnalyzer();
+            GameState state = new GameState();
+
+            state.Table[0, 0] = TileStates.O;
+            state.Table[0, 1] = TileStates.X;
+            state.Table[0, 2] = TileStates.X;
+            state.Table[1, 0] = TileStates.X;
+            state.Table[1, 1] = TileStates.O;
+            state.Table[1, 2] = TileStates.O;
+            state.Table[2, 0] = TileStates.X;
+            state.Table[2, 1] = TileStates.O;
+            state.Table[2, 2] = TileStates.X;
+
+            Players result = analyzer.DetermineWinner(state);
+
+            Assert.IsTrue(result == Players.None);
+        }
+    }
+}
--- ButterCheeseEggs/Services/Implementation/GameStateAnalyzer.cs
+++ ButterCheeseEggs/Services/Implementation/GameStateAnalyzer.cs
@@ -9,8 +9,98 @@ namespace ButterCheeseEggs.Services.Implementation
         {
             Players winner = Players.None;
 
+            if (HasEmptyTiles(state))
+            {
+                winner = Players.None;
+            }
+
+            if (IsRowInWinningConditionForX(state) || IsColumnInWinningConditionForX(state))
+            {
+                winner = Players.X;
+            }
+            if (IsRowInWinningConditionForO(state) || IsColumnInWinningConditionForO(state))
+            {
+                winner = Players.O;
+            }
+            if (IsGameATie(state))
+            {
+                winner = Players.None;
+            }
 
             return winner;
+        }
+
+
+        private bool IsRowInWinningConditionForX(GameState state)
+        {
+            bool isFirstRowX = (state.Table[0, 0] == TileStates.X) && (state.Table[1,0] == TileStates.X) && (state.Table[2,0] == TileStates.X);
+            bool isSecondRowX = (state.Table[0,1] == TileStates.X) && (state.Table[1,1] == TileStates.X) && (state.Table[2,1] == TileStates.X);
+            bool isThirdRowX = (state.Table[0,2] == TileStates.X) && (state.Table[1,2] == TileStates.X) && (state.Table[2,2] == TileStates.X);
+
+            if (isFirstRowX || isSecondRowX || isThirdRowX)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private bool IsRowInWinningConditionForO(GameState state)
+        {
+            bool isFirstRowO = (state.Table[0, 0] == TileStates.O) && (state.Table[1, 0] == TileStates.O) && (state.Table[2, 0] == TileStates.O);
+            bool isSecondRowO = (state.Table[0, 1] == TileStates.O) && (state.Table[1, 1] == TileStates.O) && (state.Table[2, 1] == TileStates.O);
+            bool isThirdRowO = (state.Table[0, 2] == TileStates.O) && (state.Table[1, 2] == TileStates.O) && (state.Table[2, 2] == TileStates.O);
+
+            if (isFirstRowO || isSecondRowO || isThirdRowO)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private bool IsColumnInWinningConditionForX(GameState state)
+        {
+
+            bool isFirstColumnX = (state.Table[0, 0] == TileStates.X) && (state.Table[0,1] == TileStates.X) && (state.Table[0,2] == TileStates.X);
+            bool isSecondColumnX = (state.Table[1,0] == TileStates.X) && (state.Table[1, 1] == TileStates.X) && (state.Table[1,2] == TileStates.X);
+            bool isThirdColumnX = (state.Table[2,0] == TileStates.X) && (state.Table[2,1] == TileStates.X) && (state.Table[2, 2] == TileStates.X);
+
+            if (isFirstColumnX || isSecondColumnX || isThirdColumnX)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private bool IsColumnInWinningConditionForO(GameState state)
+        {
+
+            bool isFirstColumnO = (state.Table[0, 0] == TileStates.O) && (state.Table[0, 1] == TileStates.O) && (state.Table[0, 2] == TileStates.O);
+            bool isSecondColumnO = (state.Table[1, 0] == TileStates.O) && (state.Table[1, 1] == TileStates.O) && (state.Table[1, 2] == TileStates.O);
+            bool isThirdColumnO = (state.Table[2, 0] == TileStates.O) && (state.Table[2, 1] == TileStates.O) && (state.Table[2, 2] == TileStates.O);
+
+            if (isFirstColumnO || isSecondColumnO || isThirdColumnO)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private bool IsGameATie(GameState state)
+        {
+            if (state.Winner == Players.None && !HasEmptyTiles(state))
+            {
+                return true;
+            }
+
+            return false;
         }
 
 
